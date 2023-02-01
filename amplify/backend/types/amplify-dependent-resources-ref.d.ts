39186987583d97ae335ab47cd15c8087cb6dd809
sudhakarmako <sudhakar@makoitlab.com>
2023-02-01 18:32:29 +0530

    amplify push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "lexkendra": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "interactions": {
+        "lexfd7af2f2": {
+            "Region": "string",
+            "BotName": "string",
+            "FunctionArn": "string"
+        }
+    }
+}
