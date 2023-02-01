79fc6a242ca0f2c7ef47dfb9d61d9a517d23903b
sudhakarmako <sudhakar@makoitlab.com>
2023-02-01 16:22:26 +0530

    removing lex

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,4 @@
-import LexChat from "react-lex-plus";
 import "./App.css";
-// function App() {
-//   return (
-//     <LexChat
-//       botName="BookTrip"
-//       IdentityPoolId="us-east-1:7ab79ccc-ea35-4fb3-a762-d18def96b951"
-//       placeholder="Write your message here"
-//       backgroundColor="#FFFFFF"
-//       height="430px"
-//       region="us-east-1"
-//       headerText="lex chat bot"
-//       headerStyle={{ backgroundColor: "#212121", fontSize: "16px" }}
-//       greeting={
-//         "Hello, how can I help? You can say things like 'help' to get more info"
-//       }
-//     />
-//   );
-// }
 
 function App() {
   return (
