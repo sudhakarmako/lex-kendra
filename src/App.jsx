4ca7431be070cbc536d55f88144ee2d857a85f0e
sudhakarmako <sudhakar@makoitlab.com>
2023-02-01 19:21:27 +0530

    chat bot sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,70 @@
+import { useRef, useState } from "react";
+import dayjs from "dayjs";
 import "./App.css";
 
 function App() {
+  const date = new Date();
+  const conversationWindow = useRef(null);
+  const [messageEntered, setMessageEntered] = useState("");
+  const [conversation, setConversation] = useState([
+    {
+      message: "Welcome to lex bot",
+      by: "lex",
+      date: date.toISOString(),
+    },
+    {
+      message: "Please type your message..",
+      by: "lex",
+      date: date.toISOString(),
+    },
+  ]);
+
+  const scrollToBottom = () => {
+    conversationWindow.current.scrollTop =
+      conversationWindow.current.scrollHeight;
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const message = {
+      message: messageEntered,
+      by: "user",
+      date: date.toISOString(),
+    };
+    setConversation((prevState) => [...prevState, message]);
+    setMessageEntered("");
+    scrollToBottom();
+  };
+
   return (
     <div className="container">
       <div className="chat-container">
         <div className="chat-header">
           <h3>Lex Kendra Chat Bot</h3>
         </div>
-        <div className="chat-messages">
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-left">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="chat-bubble chat-right">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+        <div className="chat-messages" ref={conversationWindow}>
+          {!!conversation.length &&
+            conversation.map(({ message, date, by }, idx) => (
+              <div
+                key={idx}
+                className={`chat-bubble ${
+                  by === "lex" ? "chat-left" : "chat-right"
+                }`}
+              >
+                <p>{message}</p>
+                <sub>{dayjs(date).format("HH:mm a")}</sub>
+              </div>
+            ))}
         </div>
         <div className="chat-form">
-          <form>
+          <form onSubmit={(e) => onSubmit(e)}>
             <input
               type="text"
               name="message"
               id="message"
               placeholder="Write your message here.."
+              value={messageEntered}
+              onChange={(e) => setMessageEntered(e.target.value)}
             />
             <button type="submit">Send</button>
           </form>
